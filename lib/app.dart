98c38b19cba4c7e6411c6d13a595fea3c757a850
Vijay Menon <vsm@google.com>
2024-12-19 19:57:07 -0800

    Added jaspr template

---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+import 'package:jaspr/jaspr.dart';
+
+import 'pages/about.dart';
+import 'pages/home.dart';
+
+// The main component of your application.
+class App extends StatelessComponent {
+  const App({super.key});
+
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    // This method is rerun every time the component is rebuilt.
+    //
+    // Each build method can return multiple child components as an [Iterable]. The recommended approach
+    // is using the [sync* / yield] syntax for a streamlined control flow, but its also possible to simply
+    // create and return a [List] here.
+
+    // Renders a <div class="main"> html element with children.
+    yield div(classes: 'main', [
+      const Home(),
+      const About(),
+    ]);
+  }
+}
